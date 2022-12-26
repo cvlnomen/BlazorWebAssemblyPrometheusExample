b0f82929c642374f869a3d063038e2924b3d7a7f
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-26 13:22:01 +0100

    Squashed commit of the following:
    commit a5b495f17a17e65c8ecbfb029eb91ffe27a81189
    Author: Chris <54854679+cvlnomen@users.noreply.github.com>
    Date:   Mon Dec 26 13:20:51 2022 +0100
    
        Added .json file
    
    commit 0f5acda4e7bddd885f7bac8a794b7b8cb645e788
    Author: Chris <54854679+cvlnomen@users.noreply.github.com>
    Date:   Mon Dec 26 12:21:55 2022 +0100
    
        Make dockercompose the default project
    
    commit 7fbaad70377860fcadb26a9c5a768900da9281de
    Author: Chris <54854679+cvlnomen@users.noreply.github.com>
    Date:   Mon Dec 26 12:19:15 2022 +0100
    
        Remove old launch settings
    
    commit a7b42803a3b753ddc073bc60c7dbf97406baf206
    Author: Chris <54854679+cvlnomen@users.noreply.github.com>
    Date:   Mon Dec 26 12:03:09 2022 +0100
    
        Added RandomNumberController
    
    commit 16fff0617fe146aa057fe6a5f8f169c143abf5f9
    Author: Chris <54854679+cvlnomen@users.noreply.github.com>
    Date:   Mon Dec 26 11:28:31 2022 +0100
    
        grafana
    
    commit 267b532e94d591f3fb026c9c868d60b007b72b3a
    Author: Chris <54854679+cvlnomen@users.noreply.github.com>
    Date:   Mon Dec 26 11:28:17 2022 +0100
    
        Added grafana
    
    commit a0d6b250f602e8b35c6e9651d0c99f5a34f18824
    Author: Chris <54854679+cvlnomen@users.noreply.github.com>
    Date:   Mon Dec 26 11:05:10 2022 +0100
    
        Cleanup ports
    
    commit f2962171a3c4a3eec5c573ddb2446b20daba516b
    Author: Chris <54854679+cvlnomen@users.noreply.github.com>
    Date:   Mon Dec 26 10:58:45 2022 +0100
    
        Made it work
    
    commit 6a79f184280a8d7fb146645851377cd84687c1ff
    Author: Chris <54854679+cvlnomen@users.noreply.github.com>
    Date:   Sun Dec 25 23:17:30 2022 +0100
    
        Made prometheus work alongside blazor
    
    commit 903c8cfb9bd004abb7537cc912208844f38a8f6b
    Author: Chris <54854679+cvlnomen@users.noreply.github.com>
    Date:   Sun Dec 25 22:17:32 2022 +0100
    
        1

---
+++ Server/Controllers/RandomNumberController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWebAssemblyPrometheus.Server.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class RandomNumberController : ControllerBase
+	{
+		private readonly ILogger<RandomNumberController> _logger;
+
+		public RandomNumberController(ILogger<RandomNumberController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public ActionResult<int> Get()
+		{
+			if (Random.Shared.Next(1, 10) > 5)
+			{
+				return Unauthorized();
+			}
+
+			return Random.Shared.Next();
+		}
+
+	}
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -33,63 +33,14 @@ namespace BlazorWebAssemblyPrometheus.Server
 			app.UseStaticFiles();
 
 			app.UseRouting();
-			app.UseHttpMetrics(options =>
-			{
-				// This will preserve only the first digit of the status code.
-				// For example: 200, 201, 203 -> 2xx
-				options.ReduceStatusCodeCardinality();
-			});
-			//TODO mogelijk moet ik dit in de app.UseEndpoints() zetten
+			app.UseHttpMetrics();
 			app.MapMetrics();
-
 
 			app.MapRazorPages();
 			app.MapControllers();
 			app.MapFallbackToFile("index.html");
 
 			app.Run();
-
-
-			//var builder = WebApplication.CreateBuilder(args);
-
-			//// Add services to the container.
-
-			//builder.Services.AddControllersWithViews();
-			//builder.Services.AddRazorPages();
-
-			//var app = builder.Build();
-
-			//// Configure the HTTP request pipeline.
-			//if (app.Environment.IsDevelopment())
-			//{
-			//    app.UseWebAssemblyDebugging();
-			//}
-			//else
-			//{
-			//    app.UseExceptionHandler("/Error");
-			//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-			//    app.UseHsts();
-			//}
-
-			//app.UseHttpsRedirection();
-			//app.UseRouting();
-			//app.UseHttpMetrics(options =>
-			//{
-			//    // This will preserve only the first digit of the status code.
-			//    // For example: 200, 201, 203 -> 2xx
-			//    options.ReduceStatusCodeCardinality();
-			//});
-			//app.MapMetrics();
-			////app.UseMetricServer();
-
-			//app.UseBlazorFrameworkFiles();
-			//app.UseStaticFiles();
-
-			//app.MapRazorPages();
-			//app.MapControllers();
-			//app.MapFallbackToFile("index.html");
-
-			//app.Run();
 		}
 	}
 }
