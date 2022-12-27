c34b01c9c5932ca44e4a39128eae7b36c28a62a5
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-27 12:24:17 +0100

    Added TrackingController

---
+++ Server/Controllers/TrackingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prometheus;
+
+namespace BlazorWebAssemblyPrometheus.Server.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class TrackingController : ControllerBase
+	{
+		private static readonly Counter PageVisitCounter = Metrics.CreateCounter("total_page_visits", "Number of times users have visited a page.");
+
+		private readonly ILogger<TrackingController> _logger;
+
+		public TrackingController(ILogger<TrackingController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpPost]
+		[Route("PageVisited")]
+		public ActionResult Post()
+		{
+			PageVisitCounter.Inc();
+			return Ok();
+		}
+	}
+}
